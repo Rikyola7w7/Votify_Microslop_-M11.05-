9b009975139cff289ca49537f69aa99082ffc906
Rubén Muñoz Quirós <RMUOQUI@upv.edu.es>
2026-05-22 09:09:59 +0200

    fixes

--- src/main/java/com/microslop/base/ui/MainLayout.java
+++ src/main/java/com/microslop/base/ui/MainLayout.java
@@ -16,6 +16,7 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.menubar.MenuBarVariant;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
@@ -156,17 +157,6 @@ private void rebuildUserMenu() {
             userService.logout();
             getUI().ifPresent(ui -> ui.navigate("login"));
             Notification.show(localizationService.t("profile.sessionclosed"), 3000, Notification.Position.TOP_CENTER);
-        }
-    }
-
-        userMenuContainer.add(userMenu);
-    }
-
-    private void handleLogout() {
-        if (userService != null) {
-            userService.logout();
-            getUI().ifPresent(ui -> ui.navigate("login"));
-            Notification.show("Logged out successfully", 3000, Notification.Position.TOP_CENTER);
         }
     }
 
